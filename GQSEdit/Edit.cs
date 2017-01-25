a839d9de6f9a72023d4b4aa1d6330e3035a03063
kiessler <jonas.kiessler@st.ovgu.de>
2017-01-25 02:24:14 +0100

    Added time to the editor

--- GQSEdit/Edit.cs
+++ GQSEdit/Edit.cs
@@ -9,31 +9,37 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using GQLib;
+using System.Numerics;
 
 
 namespace GQSEdit
 {
     class Edit : IState
     {
+        private int scrollValue = 0;
         private string location;
         private Texture2D bg, select, patterns;
         private Button selectButton, patternsButton, saveButton;
         private int currentState = 0;
+        private int timeSteps = 5;
 
         //General attributes
-        private String title = "test title";
-        private String subtitle = "test subtitle";
-        private String audioFile = "test audio.mp3";
-        private int stageLeadIn = 2000;
+        private string title = "test title";
+        private string subtitle = "test subtitle";
+        private string audioFile = "test audio.mp3";
+        private static int stageLeadIn = 2000;
 
         //Design attributes
         private String background = "bg.png";
 
+        //Object attributes
+        private BigInteger currentTime = stageLeadIn;
 
         public Edit(ContentManager Content, string location)
         {
             this.location = location;
             if (!Directory.Exists(location)) Directory.CreateDirectory(location);
+            this.scrollValue = Mouse.GetState().ScrollWheelValue;
 
             LoadContent(Content);
         }
@@ -52,17 +58,23 @@ namespace GQSEdit
             spriteBatch.Draw(bg, new Vector2(0), Color.White);
             selectButton.Draw(spriteBatch);
             patternsButton.Draw(spriteBatch);
-            spriteBatch.DrawString(G.Instance.font,"" + currentState, new Vector2(900, 10), Color.White);
+            //spriteBatch.DrawString(G.Instance.font,"" + currentState, new Vector2(900, 10), Color.White);
+            spriteBatch.DrawString(G.Instance.font, "Time: " + currentTime, new Vector2(850, 20), Color.White);
         }
 
         public void Update(GameTime gameTime)
         {
             selectButton.Check(Mouse.GetState());
             if (selectButton.Check(Mouse.GetState())) currentState = 0;
-            if (patternsButton.Check(Mouse.GetState())) currentState = 1;
+            if (patternsButton.Check(Mouse.GetState())) addPolar();
 
 
+            //timeline scrolling
+            if (Mouse.GetState().ScrollWheelValue > scrollValue) { currentTime += timeSteps; }
+            if (Mouse.GetState().ScrollWheelValue < scrollValue) { if (currentTime >= stageLeadIn + timeSteps) { currentTime -= timeSteps; } }
+            scrollValue = Mouse.GetState().ScrollWheelValue;
 
+            //write the .gqs file
             if (Keyboard.GetState().IsKeyDown(Keys.S) && Keyboard.GetState().IsKeyDown(Keys.LeftControl))
             {
                 using (StreamWriter streamWriter = new StreamWriter(new FileStream(location + "\\stage.gqs", FileMode.Create)))
@@ -81,6 +93,10 @@ namespace GQSEdit
                     streamWriter.WriteLine("");
                 }
             }
+        }
+
+        private void addPolar()
+        {
 
         }
     }
